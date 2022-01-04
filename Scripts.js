f44bfa06e4d1a03ccd179943c77abc4c16316ba1
HIN18002429 <HIN18002429@C-010060>
2022-01-04 09:56:20 +0000

    Added alert and prompt javascript stuff

--- Scripts.js
+++ Scripts.js
@@ -3,3 +3,11 @@ var lmao = 3 //Do not have to declare every detail of a variable like in C#
 console.log("Hello!") //Print command
 console.log(4+4)
 console.log(lmao)
+console.log(`hi ${lmao}`) //Marks used are from the button left of the number '1', the weird button
+
+alert("Welcome to my epic webpage!!!!!") //Shows up as soon as the webpage is loaded
+
+prompt("Enter your name!") //Asks the user to enter an input
+
+var name = prompt("Please enter your name")
+console.log(name)
