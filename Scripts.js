c63742ab13f1053ebefa4b0d390a147eff5b8c94
HIN18002429 <HIN18002429@C-010060>
2022-01-04 10:06:20 +0000

    user inputs 2 numbers and it outputs the sum of the numbers to the website

--- Scripts.js
+++ Scripts.js
@@ -7,7 +7,12 @@ console.log(`hi ${lmao}`) //Marks used are from the button left of the number '1
 
 alert("Welcome to my epic webpage!!!!!") //Shows up as soon as the webpage is loaded
 
-prompt("Enter your name!") //Asks the user to enter an input
+//prompt("Enter your name!") //Asks the user to enter an input
 
-var name = prompt("Please enter your name")
-console.log(name)
+//var name = prompt("Please enter your name")
+//console.log(name) //Prints the users input to the console
+
+var number1 = prompt("Please enter your first number number")
+var number2 = prompt("Please enter the second number")
+var result = Number(number1) + Number(number2)
+alert(`The result of your calculation is: ${result}`)
